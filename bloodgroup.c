810c22746ad7faf9bed9461e1a10c30ceb4c521f
eagle <pongpatsitiputa@gmail.com>
2019-11-19 17:24:39 +0700

    update bloodgroup

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,7 +5,7 @@ void main()
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf(":( What is your Name? ");
     scanf("%s", Name);
     do
     {
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye eiei\n");
+  printf("Goodbye eiei uiui\n");
 }
